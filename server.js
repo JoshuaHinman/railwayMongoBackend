8feaa3def885d6fad4ef94872c20c3205efd9ecf
Joshua Hinman <joshuahinman@gmail.com>
2023-01-16 17:30:40 -0800

    changed PORT variable

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ import mongoose from 'mongoose';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = `0.0.0.0:${process.env.PORT}` || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to database"));
 
-app.listen(PORT, () => console.log(process.env.DATABASE_URL));
+app.listen(PORT, () => console.log(PORT));
