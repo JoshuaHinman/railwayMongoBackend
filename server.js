4c16867080f73a8db41b6e9d3eae8f2054c86b23
Joshua Hinman <joshuahinman@gmail.com>
2023-01-18 11:50:33 -0800

    listening on PORT

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import EmployeeRouter from './routes/EmployeeRouter.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT ? `0.0.0.0:${process.env.PORT}` : 3000;
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -15,5 +15,5 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log("Connected to database"));
 
 app.use('/', EmployeeRouter);
-app.listen(PORT, () => console.log(PORT));
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
