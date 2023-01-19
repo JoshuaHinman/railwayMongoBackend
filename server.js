4e54ab75485178f0349e11ded48ef9eb7d99ef6a
Joshua Hinman <joshuahinman@gmail.com>
2023-01-19 13:26:15 -0800

    added api doc

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import EmployeeRouter from './routes/EmployeeRouter.js';
@@ -18,8 +19,15 @@ db.once('open', () => console.log("Connected to database"));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./'));
 
-app.get('/', (req, res) => res.status(200).send('Welcome to this API DOC'));
+const sendApiDoc = (req, res) => {
+  res.sendFile('apiDoc.html', { root: path.dirname('')});
+}
+
+app.get('/', sendApiDoc);
+app.get('/api/', sendApiDoc);
+
 app.use('/api/Employee', EmployeeRouter);
 app.use("/api/Role", RoleRouter);
 app.use("/api/TimeOffRequest", TimeOffRequestRouter);
